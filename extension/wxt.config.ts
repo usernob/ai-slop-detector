2e61597979e821fc1748116762afdf2d4d3404b3
Ramzi Pandu M <contact.ramzipandu@gmail.com>
2026-06-05 11:51:32 +0700

    init extension (#33)

---
+++ extension/wxt.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "wxt";
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+	srcDir: "src",
+	manifest: {
+		permissions: ["contextMenus", "notifications"],
+	},
+});
